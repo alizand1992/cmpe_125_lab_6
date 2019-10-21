b8cec528dce03561ce0b00167029c3421ccce48b
Ali Zand <ali.zand@sjsu.edu>
2019-10-21 15:28:23 -0700

    TASK 1 COMPLETED

--- task_1/task_1.srcs/sim_1/new/regfile2_tb.v
+++ task_1/task_1.srcs/sim_1/new/regfile2_tb.v
@@ -1,12 +1,64 @@
 module regfile2_tb;
-    reg clk;
+    reg clk, we1, we2;
+    reg [4:0] ra1, ra2, wa1, wa2;
+    reg [31:0] wd1, wd2;
+    wire [31:0] rd1, rd2;
+    
+    integer value, i;
+
+    regfile2 DUT(
+        .clk(clk), .ra1(ra1), .ra2(ra2), .wa1(wa1), 
+        .wa2(wa2), .rd1(rd1), .rd2(rd2), .wd1(wd1), 
+        .wd2(wd2), .we1(we1), .we2(we2));
     
     initial
     begin
+        // Initialize
+        clk = 0;
+        wa1 = 0;
+        wa2 = 0;
+        we1 = 0;
+        we2 = 0;
+
+    
+        // Edge cases:
+        // ra1 = ra2 = wa1
+        value = 'h1111;
+        wd1 = value;
+        we1 = 1;
+        wa1 = 5'b00010;
+        ra1 = 5'b00010;
+        ra2 = 5'b00010;
+        #4;
         
+        // Standard Test
+        for (i = 0; i < 31; i = i + 1) 
+        begin
+            wa1 = i;
+            wa2 = i + 1;
+            
+            wd1 = value;
+            value = value + 1;
+            wd2 = value;
+            we1 = ~we1;
+            we2 = ~we2;
+            ra1 = wa1;
+            ra2 = wa2;
+            #4;
+        end 
+        
+        we1 = 1;
+        we2 = 1;
+        wa1 = 1;
+        wa2 = 1;
+        ra1 = 1;
+        ra2 = 1;
+        
+        wd1 = 123;
+        wd2 = $random;
+        #4;
     end
 
-    always@(*)
-        clk = #5 ~clk; 
-    
+    always
+        #2 clk = ~clk; 
 endmodule
--- task_1/task_1.srcs/sources_1/new/regfile2.v
+++ task_1/task_1.srcs/sources_1/new/regfile2.v
@@ -8,7 +8,7 @@ module regfile2(
     
     always @(posedge clk)
     begin 
-        if (we1 != we2)
+        if (wa1 != wa2)
         begin
             if (we1)
                 reg_data[wa1] <= wd1;
@@ -17,7 +17,7 @@ module regfile2(
                 reg_data[wa2] <= wd2;
         end        
     end 
-    
-    assign rd1 = ra1 != 0 ? reg_data[ra1] : 0;
-    assign rd2 = ra2 != 0 ? reg_data[ra2] : 0;
+  
+    assign rd1 = (ra1 != 0) ? reg_data[ra1] : 0;
+    assign rd2 = (ra2 != 0) ? reg_data[ra2] : 0;
 endmodule
